4718cbc0f72150cecfa66aeecca4e79f43c45e16
dmitry_i <fstpain@gmail.com>
2023-02-26 22:36:12 +0300

    Add Add solution Calculate average

--- Calculate average.js
+++ Calculate average.js
@@ -1,6 +1,12 @@
-// Complete the solution so that it reverses the string passed into it.
-
-function solution(str){
-    return str.split('').reverse().join('');
-    
-}
+// Write a function which calculates the average of the numbers in a given list.
+// Note: Empty arrays should return 0.
+function findAverage(array) {
+    if(array.length !== 0){
+      let result = 0
+       for(let elem of array){
+         result+=elem;
+       }
+      return result/array.length
+    }
+    return 0;
+  }
