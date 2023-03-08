44b2b94fb31f0670989830dc3be03bef7bf5a7d6
dmitry_i <fstpain@gmail.com>
2023-03-08 23:47:45 +0300

    add Find the unique number

---
+++ Find the unique number.js
@@ -0,0 +1,8 @@
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+function findUniq(arr) {
+  
+  arr.sort((a,b)=>a-b)
+  console.log(arr)
+  return arr[0] == arr[1]? arr.pop(): arr.shift();
+}
