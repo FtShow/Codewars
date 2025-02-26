f536f97572fb33df5b26993649ec3b1730de2b15
Dmitriy <fstpain@gmail.com>
2025-02-26 18:45:13 +0300

    getDivisorsCnt

---
+++ Count the divisors of a number.js
@@ -0,0 +1,11 @@
+function getDivisorsCnt(n){
+    let result = 0
+    for(let i = 1; i * i <= n; i++){
+        if(n%i === 0){
+            result++
+            if (i !== n / i) result++;
+        }
+
+    }
+    return result
+}
